a76e70d10fc40de27d82d44949050f469581a462
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 21:45:15 +0000

    build based on 0d14210

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoardGameGeek","category":"page"},{"location":"#BoardGameGeek","page":"Home","title":"BoardGameGeek","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoardGameGeek.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoardGameGeek]","category":"page"}]
+}
